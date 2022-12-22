8a458236c9d3f6d120c773decf5047ff791aa547
Mo salah <MSalahElrayes@gmail.com>
2022-12-22 13:26:25 +0200

    add database connection

---
+++ initial_framework/config.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'db'=> [
+        'host' => 'localhost',
+        'port' => '3306',
+        'dbname' => 'firstapp',
+        'charset' => 'utf8mb4'
+    ]
+];
--- initial_framework/index.php
+++ initial_framework/index.php
@@ -1,3 +1,19 @@
 <?php
+
+//require 'router.php';
 require 'functions.php';
-require 'router.php';
+require 'Database.php';
+
+$config = require 'config.php';
+
+$dp = new DataBase($config['db']);
+
+$id = $_GET['id'];
+
+$query = 'SELECT * FROM posts where id = :id'; // :any_word or use ?
+
+$posts = $dp->query($query, ['id' => $id])->fetchAll();
+
+foreach ($posts as $post) {
+    echo '<li>' . $post['title'] . '</li>';
+}
